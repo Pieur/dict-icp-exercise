02a78b8000a63cc03caef5ccc55b5fca7e93cfa8
Pieur <114052640+Pieur@users.noreply.github.com>
2024-06-25 15:20:17 +0800

    added whole backend folder

---
+++ backend/database/entities/index.ts
@@ -0,0 +1,6 @@
+import { Configuration } from './configuration';
+import { Event } from './event';
+import { User } from './user';
+
+
+export const ENTITIES = [User, Event, Configuration, Guest, Feedback, Notification];
